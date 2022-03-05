2a8f7070b9ae646d164c12db9ffe9354c382f488
Zian Lin <z9lin@ucsd.edu>
2022-03-05 15:19:20 -0800

    App now tracks user's current location

---
+++ app/src/main/java/com/example/zanewearableswatch/LocationService.java
@@ -0,0 +1,75 @@
+package com.example.zanewearableswatch;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class LocationService extends Service {
+    public static double latitude = 0.0;
+    public static double longitude = 0.0;
+    FusedLocationProviderClient flpc;
+    LocationCallback locationCallback;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        flpc = LocationServices.getFusedLocationProviderClient(this);
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                latitude = locationResult.getLastLocation().getLatitude();
+                longitude = locationResult.getLastLocation().getLongitude();
+                Log.d("Latitude, Longitude", String.valueOf(latitude) + String.valueOf(longitude));
+            }
+        };
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        requestLocation();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void requestLocation() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        flpc.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+    }
+}
--- app/src/main/java/com/example/zanewearableswatch/MainActivity.java
+++ app/src/main/java/com/example/zanewearableswatch/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.zanewearableswatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -25,6 +33,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
@@ -40,11 +52,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -54,22 +68,9 @@ public class MainActivity extends AppCompatActivity {
     String destination = "";
     String key = "AIzaSyAUkJ-ObhbadMLikRQZ3i2_L79WB7Fug3Q"; //add api key
     String mode = "";
+    double latitude = LocationService.latitude;
+    double longitude = LocationService.longitude;
     ArrayList<Step> steps = new ArrayList<Step>();
-
-    /*
-    //current lat and long, Work in progress
-    LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-    double longitude = location.getLongitude();
-    double latitude = location.getLatitude();
-
-
-    private final LocationListener locationListener = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
-        }
-    };*/
 
     //POJO for step data parsed from call to Directions API
     private class Step {
@@ -114,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
 
         public String toString() {
             String[] arr = {instruction, maneuver, String.valueOf(distance), String.valueOf(distance),
-                            startLocation.toString(), endLocation.toString()};
+                    startLocation.toString(), endLocation.toString()};
             return Arrays.stream(arr).collect(Collectors.joining(", "));
         }
     }
@@ -124,24 +125,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //start service that continuously gets lat and long of user
+        if ((Build.VERSION.SDK_INT >= 23) && (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        else {
+            startService();
+        }
+
         ImageButton bikeButton = findViewById(R.id.bikeButton);
         ImageButton walkButton = findViewById(R.id.walkButton);
         ImageButton carButton = findViewById(R.id.carButton);
 
-        String testOrigin = "\"ChIJyYB_SZVU2YARR-I1Jjf08F0 2920 Zoo Dr, San Diego, CA 92101, USA\"";
-
         bikeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                Log.d("User selects", "biking");
                 mode = "b";
             }
         });
         walkButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                Log.d("User selects", "walking");
                 mode = "w";
             }
         });
         carButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                Log.d("User selects", "driving");
                 mode = "d";
             }
         });
@@ -150,6 +161,8 @@ public class MainActivity extends AppCompatActivity {
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("Navigation start: current lat and long is", latitude + " " + longitude);
+                origin = latitude + "," + longitude;
                 destination = ((EditText)findViewById(R.id.destination)).getText().toString();
 
                 if (destination.equals("") || mode == "") {
@@ -164,14 +177,33 @@ public class MainActivity extends AppCompatActivity {
                 RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
                 //String request = "google.navigation:q=" + destinationText + "&mode=" + mode;
                 //for now, destination is just the textfield, but it should be coordinates
-                String url = "https://maps.googleapis.com/maps/api/directions/json?"+ "origin="
-                        + testOrigin + "&destination=" + destination
+                String url = "https://maps.googleapis.com/maps/api/directions/json?"
+                        + "origin=" + origin + "&destination=" + destination
                         + "&mode=" + mode + "&key=" + key;
 
                 JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new ResponseListener(), new ErrorListener());
                 queue.add(jsonRequest);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startService();
+                }
+                else {
+                    Toast.makeText(this, "App requires location permissions", Toast.LENGTH_LONG).show();
+                }
+        }
+    }
+
+    void startService() {
+        Intent intent = new Intent(MainActivity.this, LocationService.class);
+        startService(intent);
     }
 
     /*
@@ -185,30 +217,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }*/
 
-
-
     private class ResponseListener implements Response.Listener<JSONObject>{
         @Override
         public void onResponse(JSONObject json) {
             Log.d("Response is: ", "successful");
-            String responseStr = json.toString();
-            /*
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            map = gson.fromJson(json.toString(), new TypeToken<HashMap<String, Object>>() {}.getType());
-            //System.out.println(gson.toJson(json));
-            System.out.println("Starting gson");
-
-            JSONArray jsonArray = json.getJSONArray("response");
-
-            int[] myArray = new Gson().fromJson(jsonArray, int[].class);
-
-            System.out.println(Arrays.toString(myArray));*/
-
-            /*
-            Gson gson = new Gson();
-            MapCall mapCall = gson.fromJson(responseStr, MapCall.class);
-            System.out.println(mapCall.status);
-            */
 
             try {
                 JSONArray routesArr = json.getJSONArray("routes");
@@ -233,9 +245,9 @@ public class MainActivity extends AppCompatActivity {
                     step.setDistance(distance);
                     step.setDuration(duration);
                     step.setStart(new LatLng(start.getDouble("lat"),
-                                             start.getDouble("lng")));
+                            start.getDouble("lng")));
                     step.setEnd(new LatLng(end.getDouble("lat"),
-                                           end.getDouble("lng")));
+                            end.getDouble("lng")));
 
                     steps.add(step);
                 }
@@ -244,6 +256,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             catch (JSONException e) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Error")
+                        .setMessage("Invalid destination!")
+                        .setNegativeButton("OK", null)
+                        .show();
                 e.printStackTrace();
             }
         }
