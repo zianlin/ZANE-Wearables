dbe0b593a46a33459956b0a0061bff693a7fa35d
Zian Lin <z9lin@ucsd.edu>
2022-03-05 04:27:12 -0800

    JSON data now parsed into ArrayList of 'Step' objects

--- app/src/main/java/com/example/zanewearableswatch/MainActivity.java
+++ app/src/main/java/com/example/zanewearableswatch/MainActivity.java
@@ -23,22 +23,38 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.google.maps.model.Distance;
+import com.google.maps.model.Duration;
+import com.google.maps.model.LatLng;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
-    EditText destination = null;
+    String origin = ""; //find a way to get the user's current location
+    String destination = "";
+    String key = "AIzaSyAUkJ-ObhbadMLikRQZ3i2_L79WB7Fug3Q"; //add api key
     String mode = "";
-    String resp = "";
+    ArrayList<Step> steps = new ArrayList<Step>();
 
     /*
     //current lat and long, Work in progress
@@ -55,6 +71,54 @@ public class MainActivity extends AppCompatActivity {
         }
     };*/
 
+    //POJO for step data parsed from call to Directions API
+    private class Step {
+        private String instruction;
+        private String maneuver;
+        private int distance;
+        private int duration;
+        private LatLng startLocation;
+        private LatLng endLocation;
+
+        public Step() {
+            instruction = "";
+            maneuver = "";
+            distance = 0;
+            duration = 0;
+            startLocation = null;
+            endLocation = null;
+        }
+
+        public Step(String ins, String man, int dis, int dur, LatLng start, LatLng end) {
+            instruction = ins;
+            maneuver = man;
+            distance = dis;
+            duration = dur;
+            startLocation = start;
+            endLocation = end;
+        }
+
+        public String getInstruction()  {return instruction;}
+        public String getManeuver()     {return maneuver;}
+        public int getDistance()        {return distance;}
+        public int getDuration()        {return duration;}
+        public LatLng getStart()        {return startLocation;}
+        public LatLng getEnd()          {return endLocation;}
+
+        public void setInstruction(String ins)  {instruction = ins;}
+        public void setManeuver(String man)     {maneuver = man;}
+        public void setDistance(int dis)        {distance = dis;}
+        public void setDuration(int dur)        {duration = dur;}
+        public void setStart(LatLng start)      {startLocation = start;}
+        public void setEnd(LatLng end)          {endLocation = end;}
+
+        public String toString() {
+            String[] arr = {instruction, maneuver, String.valueOf(distance), String.valueOf(distance),
+                            startLocation.toString(), endLocation.toString()};
+            return Arrays.stream(arr).collect(Collectors.joining(", "));
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,11 +127,8 @@ public class MainActivity extends AppCompatActivity {
         ImageButton bikeButton = findViewById(R.id.bikeButton);
         ImageButton walkButton = findViewById(R.id.walkButton);
         ImageButton carButton = findViewById(R.id.carButton);
-        String origin = ""; //find a way to get the user's current location
-        String key = "AIzaSyAUkJ-ObhbadMLikRQZ3i2_L79WB7Fug3Q"; //add api key
 
-        String testDestination = "\"ChIJyYB_SZVU2YARR-I1Jjf08F0 2920 Zoo Dr, San Diego, CA 92101, USA\"";
-        String testOrigin = "ChIJE9on3F3HwoAR9AhGJW_fL-I\n Los Angeles, CA, USA";
+        String testOrigin = "\"ChIJyYB_SZVU2YARR-I1Jjf08F0 2920 Zoo Dr, San Diego, CA 92101, USA\"";
 
         bikeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -89,103 +150,28 @@ public class MainActivity extends AppCompatActivity {
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //set destination and mode
-                destination  = (EditText)findViewById(R.id.destination);
-                String destinationText = destination.getText().toString();
+                destination = ((EditText)findViewById(R.id.destination)).getText().toString();
 
-                if (destinationText.equals("") || mode == "") {
+                if (destination.equals("") || mode == "") {
                     new AlertDialog.Builder(MainActivity.this)
                             .setTitle("Error")
                             .setMessage("Destination or mode has not been set!")
-
-                            // A null listener allows the button to dismiss the dialog and take no further action.
                             .setNegativeButton("OK", null)
                             .show();
+                    return;
                 }
 
                 RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
                 //String request = "google.navigation:q=" + destinationText + "&mode=" + mode;
                 //for now, destination is just the textfield, but it should be coordinates
                 String url = "https://maps.googleapis.com/maps/api/directions/json?"+ "origin="
-                        + testOrigin + "&destination=" + testDestination
+                        + testOrigin + "&destination=" + destination
                         + "&mode=" + mode + "&key=" + key;
 
-                //probably don't need this
-                /*JSONObject jsonObject = new JSONObject();
-                try {
-                    jsonObject.put("google.destination.q", destinationText);
-                    jsonObject.put("&mode", mode);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                 */
-
-
-                //System.out.println(request); //for testing
-                //Toast.makeText(MainActivity.this, url, Toast.LENGTH_LONG).show();
-                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,  new ResponseListener(), new ErrorListener());
-                ArrayList<Bundle> bundleList = parse(resp);
-
-                for (Bundle bundle : bundleList)
-                {
-                    for (String key: bundle.keySet())
-                    {
-                        Log.d("Bundle Debug", key + " = \"" + bundle.get(key) + "\"");
-                    }
-                }
-
-
-                queue.add(request);
-                /* this code probably isn't what we're looking for, it just opens a separate map.
-                 * including in case its useful
-                Intent intent = new Intent(Intent.ACTION_VIEW,
-                                Uri.parse("google.navigation:q=0,0&mode=d"));
-                intent.setPackage("com.google.android.apps.maps");
-
-                //google maps downloaded
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }
-                else { //google maps not downloaded
-                    new AlertDialog.Builder(this)
-                            .setTitle("Error")
-                            .setMessage("Application requires Google Maps to be downloaded on the device.")
-
-                            // A null listener allows the button to dismiss the dialog and take no further action.
-                            .setNegativeButton(android.R.string.no, null)
-                            .setIcon(android.R.drawable.ic_dialog_alert)
-                            .show();
-                }*/
+                JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new ResponseListener(), new ErrorListener());
+                queue.add(jsonRequest);
             }
         });
-    }
-    public ArrayList<Bundle> parse(String response){
-        ArrayList<Bundle> list = new ArrayList<Bundle>();
-        try {
-            JSONObject json = new JSONObject(response);
-            JSONArray routes = json.getJSONArray("route");
-            JSONArray legs = routes.getJSONArray(0);
-            JSONArray steps = legs.getJSONArray(0);
-            for(int i=0;i<steps.length();i++) {
-                JSONObject singleStep = steps.getJSONObject(i);
-                JSONObject duration = singleStep.getJSONObject("duration");
-                Bundle dur = new Bundle();
-                dur.putString("text", duration.getString("text"));
-                dur.putString("value", duration.getString("value"));
-                JSONObject distance = singleStep.getJSONObject("distance");
-                Bundle dis = new Bundle();
-                dis.putString("text", distance.getString("text"));
-                dis.putString("value", distance.getString("value"));
-                Bundle data = new Bundle();
-                data.putBundle("duration", dur);
-                data.putBundle("distance", dis);
-                list.add(data);
-            }
-        } catch (JSONException e1) {
-            e1.printStackTrace();
-        }
-        return list;
     }
 
     /*
@@ -200,21 +186,73 @@ public class MainActivity extends AppCompatActivity {
     }*/
 
 
+
     private class ResponseListener implements Response.Listener<JSONObject>{
         @Override
-        public void onResponse(JSONObject response) {
-            Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_LONG).show();
-            destination.setText(response.toString());
-            resp = response.toString();
-            System.out.println(response.toString());
+        public void onResponse(JSONObject json) {
+            Log.d("Response is: ", "successful");
+            String responseStr = json.toString();
+            /*
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            map = gson.fromJson(json.toString(), new TypeToken<HashMap<String, Object>>() {}.getType());
+            //System.out.println(gson.toJson(json));
+            System.out.println("Starting gson");
+
+            JSONArray jsonArray = json.getJSONArray("response");
+
+            int[] myArray = new Gson().fromJson(jsonArray, int[].class);
+
+            System.out.println(Arrays.toString(myArray));*/
+
+            /*
+            Gson gson = new Gson();
+            MapCall mapCall = gson.fromJson(responseStr, MapCall.class);
+            System.out.println(mapCall.status);
+            */
+
+            try {
+                JSONArray routesArr = json.getJSONArray("routes");
+                JSONObject routesObj = routesArr.getJSONObject(0);
+                JSONArray legsArr = routesObj.getJSONArray("legs");
+                JSONObject legsObj = legsArr.getJSONObject(0);
+                JSONArray stepsArr = legsObj.getJSONArray("steps");
+
+                for (int i = 0; i < stepsArr.length(); i++) {
+                    Step step = new Step();
+                    JSONObject stepObj = stepsArr.getJSONObject(i);
+
+                    String instruction = stepObj.getString("html_instructions");
+                    String maneuver = stepObj.has("maneuver") ? stepObj.getString("maneuver") : "";
+                    int distance = stepObj.getJSONObject("distance").getInt("value");
+                    int duration = stepObj.getJSONObject("duration").getInt("value");
+                    JSONObject start = stepObj.getJSONObject("start_location");
+                    JSONObject end = stepObj.getJSONObject("end_location");
+
+                    step.setInstruction(instruction);
+                    step.setManeuver(maneuver);
+                    step.setDistance(distance);
+                    step.setDuration(duration);
+                    step.setStart(new LatLng(start.getDouble("lat"),
+                                             start.getDouble("lng")));
+                    step.setEnd(new LatLng(end.getDouble("lat"),
+                                           end.getDouble("lng")));
+
+                    steps.add(step);
+                }
+                for (int i = 0; i < steps.size(); i++) { //debug
+                    Log.d(String.valueOf(i), steps.get(i).toString());
+                }
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     private class ErrorListener implements Response.ErrorListener{
         @Override
         public void onErrorResponse(VolleyError error) {
-
-            Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+            Log.e("Response is: ", "error");
         }
     }
 }
